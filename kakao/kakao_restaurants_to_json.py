c3ce3958e2a82182400befb42fa44a14557899ed
lsc713 <lsc7134@naver.com>
2024-08-16 04:15:40 +0900

    KAN-156 카카오 운영시간 JSON변환

--- kakao/kakao_restaurants_to_json.py
+++ kakao/kakao_restaurants_to_json.py
@@ -1,16 +1,13 @@
 import pandas as pd
-import json
 
-# Load the CSV file
-csv_file_path = 'kakao_restaurants_updated.csv'
-df = pd.read_csv(csv_file_path)
+# CSV 파일을 읽어옵니다.
+data = pd.read_csv('kakao_restaurants.csv')
 
-# Convert each row to a JSON object and store in a list
-json_list = df.to_dict(orient='records')
+# DataFrame을 JSON 형식으로 변환합니다.
+data_json = data.to_json(orient='records', force_ascii=False, indent=4)
 
-# Save the list of JSON objects to a file
-json_file_path = 'kakao_restaurants_updated1.json'
-with open(json_file_path, 'w', encoding='utf-8') as json_file:
-  json.dump(json_list, json_file, ensure_ascii=False, indent=4)
+# JSON 데이터를 파일로 저장합니다.
+with open('kakao_restaurants.json', 'w', encoding='utf-8') as f:
+  f.write(data_json)
 
-print(f"JSON file has been saved to {json_file_path}")
+print("JSON 파일이 생성되었습니다.")
