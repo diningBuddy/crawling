08e0fdb99e9211365ba41ce08124d835e152be51
이주호 <139448668+lsc713@users.noreply.github.com>
2024-12-03 00:58:28 +0900

    KAN-263 feat:kakao_restaurants.py (#13)

--- kakao/core/kakao_restaurants.py
+++ kakao/core/kakao_restaurants.py
@@ -17,14 +17,14 @@ csv_file = open('../data/kakao_restaurants.csv', mode='w', newline='', encoding=
 csv_writer = csv.writer(csv_file)
 csv_writer.writerow(['id', 'name', 'category', 'review_count', 'address', 'rating', 'rating_count', 'phone_number', 'operate_time', 'url'])
 
+# 카테고리 저장을 위한 set
+categories_set = set()
 
 # Selenium WebDriver 설정
 options = Options()
 options.add_argument("--disable-blink-features=AutomationControlled")
-
 user_agent = 'user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
 options.add_argument(user_agent)
-
 options.add_experimental_option('excludeSwitches', ['enable-logging', 'enable-automation'])
 options.add_experimental_option("useAutomationExtension", False)
 options.add_argument("--remote-allow-origins=*")
@@ -38,7 +38,6 @@ url = "https://map.kakao.com/"
 driver.get(url)
 time.sleep(6)
 
-
 # 검색어 입력 및 검색 실행
 search_box = driver.find_element(By.CSS_SELECTOR, "input[id='search.keyword.query']")
 search_keyword = "성균관대역 음식점"
@@ -50,7 +49,6 @@ time.sleep(1)
 # 위치 고정
 location_fix = driver.find_element(By.CSS_SELECTOR, "span[id='search.keyword.currentmap']")
 driver.execute_script("arguments[0].click();", location_fix)
-
 
 # 기존 CSV 파일에서 식당 이름 목록 가져오기
 naver_map_crawling_path = '../../naver/restaurants.csv'
@@ -80,6 +78,9 @@ for name in restaurant_names:
   operate_time_element = driver.find_element(By.XPATH, "//a[contains(@data-id, 'periodTxt')]")
   detail_link_element = driver.find_element(By.XPATH, "//a[@class='moreview']")
   detail_link = detail_link_element.get_attribute('href')
+
+  # 카테고리 저장
+  categories_set.add(category_element.text)
 
   # 평가 수 텍스트 처리
   rating_eval_count_element_text = ''
@@ -134,6 +135,9 @@ if os.path.exists(kakao_map_ranks_path):
     detail_link_element = driver.find_element(By.XPATH, "//a[@class='moreview']")
     detail_link = detail_link_element.get_attribute('href')
 
+    # 카테고리 저장
+    categories_set.add(category_element.text)
+
     rating_eval_count_element_text = ''
     if '건' in rating_eval_count_element.text:
       rating_eval_count_element_text = rating_eval_count_element.text.replace('건', '').strip()
@@ -161,7 +165,12 @@ csv_file.close()
 
 df = pd.read_csv('../data/kakao_restaurants.csv')
 df.drop_duplicates(subset=['name', 'address'], keep='first', inplace=True)
-df.to_csv('kakao_restaurants.csv', index=False, encoding='utf-8-sig')
+df.to_csv('../data/kakao_restaurants.csv', index=False, encoding='utf-8-sig')
+
+# 카테고리 set을 텍스트 파일로 저장
+with open('../data/kakao_categories.txt', 'w', encoding='utf-8') as f:
+  for category in sorted(categories_set):
+    f.write(f"{category}\n")
 
 # Selenium 드라이버 종료
 driver.quit()
